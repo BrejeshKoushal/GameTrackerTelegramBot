4549d19c137b5353af995f56bbb07b87392eeda3
BrejeshKoushal <brejeshkoushal28@gmail.com>
2025-08-30 03:38:52 +0530

    User and Game Entity

--- app/src/main/java/org/example/entity/Game.java
+++ app/src/main/java/org/example/entity/Game.java
@@ -1,4 +1,37 @@
 package org.example.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Slf4j
+@Entity
+@Table(name = "game")
 public class Game {
+
+    @Column(name = "game_name")
+    private String gameName;
+
+    @Id
+    @Column(name = "game_id")
+    private Long gameId;
+
+    @Column(name = "original_price")
+    private BigDecimal originalPrice;
+
+    @Column(name = "current_price")
+    private BigDecimal currentPrice;
+
+    @ManyToMany(mappedBy = "games")
+    private Set<User> users = new HashSet<>();
+
 }
--- app/src/main/java/org/example/entity/User.java
+++ app/src/main/java/org/example/entity/User.java
@@ -1,14 +1,36 @@
-package org.example.entity;
+    package org.example.entity;
 
-import jakarta.persistence.*;
-import org.springframework.context.annotation.Primary;
+    import jakarta.persistence.*;
+    import lombok.*;
+    import lombok.extern.slf4j.Slf4j;
 
-@Entity
-@Table(name = "user")
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    import java.util.HashSet;
+    import java.util.Set;
 
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    @Builder
+    @Slf4j
+    @NoArgsConstructor
+    @Entity
+    @Table(name = "user")
+    public class User {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "id")
+        private Long id;
 
-}
+        @Column(name = "password")
+        private String password;
+
+        @ManyToMany
+        @JoinTable(
+                name = "user_game",
+                joinColumns = @JoinColumn(name="user_id"),
+                inverseJoinColumns = @JoinColumn(name = "game_id")
+        )
+        private Set<Game> games = new HashSet<>();
+
+    }
+
